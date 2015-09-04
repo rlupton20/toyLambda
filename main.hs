18f939d595bc9572c5fecb415cec63672f6fcb87
Richard <richard.lupton@gmail.com>
2015-09-04 23:27:29 +0100

    Added some kind of beta reduction.

--- main.hs
+++ main.hs
@@ -4,7 +4,7 @@
 import Text.Parsec as Parsec hiding ((<|>))
 import Control.Applicative
 
-data Term a = Atom a | Abstraction (Term a) (Term a) | Application (Term a) (Term a) deriving Show
+data Term a = Atom a | Abstraction (Term a) (Term a) | Application (Term a) (Term a) deriving (Show, Eq)
 type LambdaTerm = Term String
 
 wrapSpaces :: Parsec String () a -> Parsec String () a
@@ -52,3 +52,26 @@ application = Application <$> (term <* apply) <*> term
 -- Note lambda must be escaped, and written \\	
 test :: Parsec String () a -> String -> Either ParseError a
 test par str = parse par ("Input string: " ++ str) str
+
+lambdaTerm :: String -> Either ParseError LambdaTerm
+lambdaTerm = test term
+
+-- Best used in applicative style with lambdaTerm
+substitute :: (Eq a) => Term a -> Term a -> Term a -> Term a
+substitute (Atom var) term (Atom var') = if var' == var then term else (Atom var')
+substitute atom@(Atom var) term (Application t1 t2) = (Application (substitute atom term t1) (substitute atom term t2))
+substitute atom term abstr@(Abstraction var' t) = if atom == var' then abstr else Abstraction var' $ substitute atom term t
+substitute _ _ term = term
+
+-- Again, use in an applicative style
+beta :: (Eq a) => Term a -> Term a
+beta (Atom x)  = Atom x
+beta term@(Application f s) = case f of (Abstraction x t) -> beta $ substitute x s t
+					(Application g r) -> beta $ (Application (beta $ Application g r) s)
+					_ -> term
+beta term = term
+
+-- Trying to reduce an irreducible term -- break out with Ctrl + C
+example1 = beta <$> lambdaTerm "((\\x.(x|x))|(\\x.(x|x)))"
+-- Currying
+example2 = beta <$> lambdaTerm "(((\\x.(\\y.(x|y)))|a)|b)"
